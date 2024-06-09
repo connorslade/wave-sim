ce99bccc324a7319d77c4c5aa00b7aa5b07be174
Connor Slade <connor@connorcode.com>
2024-06-08 22:33:38 -0400

    Update slit plot

--- slit_plot.jl
+++ slit_plot.jl
@@ -3,19 +3,26 @@ using Plots.PlotMeasures
 using FileIO
 using ImageIO
 
+#= CONFIG =#
 
-data = reinterpret(Float32, read("out.f32"))
+smooth = 15
+image_width = 100
+image_color = RGB(0, 1, 0)
+# image_color = RGB(0.4549019607843137, 0.1607843137254902, 0.4588235294117647)
+
+data = reinterpret(Float32, read("out.f32"))[2:end-1]
 max_value = sqrt(maximum(data))
 
-image_width = 100
-image_color = RGB(0.4549019607843137, 0.1607843137254902, 0.4588235294117647)
+for i in eachindex(data)
+	data[i] = sum(data[max(1, i - smooth):min(end, i + smooth)]) / (2 * smooth + 1)
+end
 
 image = zeros(RGB{Float64}, image_width, length(data))
 
-for i in 1:length(data)
+for i in eachindex(data)
 	value = sqrt(data[i]) / max_value
-
 	color = image_color * value
+
 	for j in 1:image_width
 		image[j, i] = color
 	end
